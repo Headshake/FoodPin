e3f338a3dfb3113cdcdc91899116974d71a86c7b
Beatrice <beatricelim188@yahoo.com>
2020-11-05 12:32:49 +0800

    Add Call, Check, Cancel Action

--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -13,6 +13,12 @@ class RestaurantTableViewController: UITableViewController {
     
     var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate", "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpubkitchen"]
 
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+    
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+    
+    var restaurantIsVisited = Array(repeating: false, count: 21)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -42,11 +48,49 @@ class RestaurantTableViewController: UITableViewController {
         // Configure the cell...
         cell.nameLabel.text = restaurantNames[indexPath.row] //optional chaining (? check if the input of the name Label is empty)
             //but in this case the nameLabel is connected to the name from the data, therefore the input will never be empty
+        cell.typeLabel.text = restaurantTypes[indexPath.row]
+        cell.locationLabel.text = restaurantLocations[indexPath.row]
         cell.thumbnailImageView.image = UIImage(named: restaurantImages[indexPath.row])
          
         return cell
     }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    // Create an option menu as an action sheet
+    let optionMenu = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: .actionSheet)
+        
+    // Add Call action
+    let callActionHandler = { (action:UIAlertAction!) -> Void in
+    let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature is not available yet. Please retry later.", preferredStyle: .alert)
+    alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+    self.present(alertMessage, animated: true, completion: nil)
+    }
+
+    let callAction = UIAlertAction(title: "Call " + "123-000-\(indexPath.row)", style: .default, handler: callActionHandler)
+    optionMenu.addAction(callAction)
+        
+    // Check-in action
+    let checkInAction = UIAlertAction(title: "Check in", style: .default, handler: {
+    (action:UIAlertAction!) -> Void in
+
+    let cell = tableView.cellForRow(at: indexPath)
+    cell?.accessoryType = .checkmark
+    self.restaurantIsVisited[indexPath.row] = true
+    })
+    optionMenu.addAction(checkInAction)
+        
+    // Deselect a row
+    tableView.deselectRow(at: indexPath, animated: false)
+        
+    // Add actions to the menu
+    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+    optionMenu.addAction(cancelAction)
+
+    // Display the menu
+    present(optionMenu, animated: true, completion: nil)
+    }
+    
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
@@ -73,7 +117,7 @@ class RestaurantTableViewController: UITableViewController {
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
     */
 
